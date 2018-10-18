54d2b2aa3ce8e3cc2ba9a39f8920fb89291b16d4
Reece Rose <reecerose18@gmail.com>
2018-10-17 20:21:47 -0400

    New Vue template

---
+++ src/components/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="Navigation">
+        <nav>
+            <h1>Reece Rose</h1>
+        </nav>
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/Projects">Projects</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default 
+{
+    name: 'Navigation',
+    props: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    $blue: red;
+    nav {
+        h1 {
+            color: $blue;
+        }
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="Home">
+        <Navigation/>
+    </div>
+</template>
+
+<script>
+
+import Navigation from '@/components/Navigation.vue'
+export default {
+    name: 'Home',
+    components: {
+        Navigation
+    }
+}
+</script>
