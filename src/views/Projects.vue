c5ec19b659cadb0a6c042f1d87123935328ef9e3
reecerose <reecerose18@gmail.com>
2019-04-06 14:01:35 -0400

    Finished firebase implementation

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,16 +2,16 @@
     <section id="Projects" class="svg-background">
         <Navigation/>
         <div class="container">
-            <div v-if="String($route.params.project).toLowerCase() != null && json.projects[String($route.params.project).toLowerCase()] != null">
+            <div v-if="selectedProjectIndex != null && String($route.params.project.toLowerCase()) != null && projects[selectedProjectIndex].name.toLowerCase().replace(/ /g, '') == String($route.params.project).toLowerCase().replace(/ /g, '')">
                 <DetailedProject
-                    :project="json.projects[String($route.params.project).toLowerCase()]"
+                    :project="projects[selectedProjectIndex]"
                 />
             </div>
             <div v-else class="section-background text-center">
                 <h1>My Projects</h1>
                 <hr>
                 <div class="row justify-content-center">
-                    <div v-for="(project, index) in json.projects" :key="index" class="col-lg-4 col-md-4 col-sm-6 col-xs-12 projects">
+                    <div v-for="(project, index) in projects" :key="index" class="col-lg-4 col-md-4 col-sm-6 col-xs-12 projects">
                         <TileProject 
                             :title="project.name"
                             :display_image="project.display_image" 
@@ -27,7 +27,6 @@
 import Navigation from '@/components/Navigation.vue'
 import TileProject from '@/components/TileProject.vue'
 import DetailedProject from '@/components/DetailedProject'
-import Projects from '@/assets/projects.json'
 
 export default {
     name: 'Projects',
@@ -36,9 +35,17 @@ export default {
         TileProject,
         DetailedProject
     },
-    data () {
-        return {
-            json: Projects
+    computed: {
+        projects() {
+            return this.$store.getters.getProjects
+        },
+        selectedProjectIndex() {
+            if (this.$route.params.project == null) return null
+            let projectName = this.$route.params.project
+            for (let index = 0; index < this.projects.length; index++) {
+                if (this.projects[index].name.toLowerCase().replace(/ /g, '') == projectName.toLowerCase().replace(/ /g, '')) return index;
+            }
+            return null
         }
     }
 }
