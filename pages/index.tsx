3aaa85650ca8b398cf84292c0dfd1437dd4f68c4
Reece Rose <reecerose18@gmail.com>
2021-02-22 22:32:58 -0500

    Now including background

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,10 @@ import SEO from '../components/SEO';
 
 export default function Home(): JSX.Element {
   return (
-    <div>
+    <div
+      className="w-full h-full min-h-screen bg-fixed bg-white bg-cover"
+      style={{ backgroundImage: "url('/background.svg')" }}
+    >
       <SEO
         title="Reece Rose - Portfolio"
         description="Portfolio website for Reece Rose"
