9bf20bc323dc052d0f3f00102a98204d5f4d3744
Reece Rose <reecerose18@gmail.com>
2023-02-06 19:08:27 -0500

    New portfolio using Astro (#34)

--- __tests__/components/Header.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-
-import Header from '../../components/Header';
-
-test('Header component matches snapshot', () => {
-  const { asFragment } = render(<Header />);
-  expect(asFragment()).toMatchSnapshot();
-});
---
+++ src/data/info.ts
@@ -0,0 +1,67 @@
+export const info = {
+  baseUrl: "https://reecerose.com",
+  name: "Reece Rose",
+  jobDescription: "Full-stack Developer",
+  about: `
+    As a software developer based in Kitchener, ON, Canada, I specialize in creating dynamic and visually appealing websites and mobile applications.
+    With a passion for technology and a keen eye for design, I strive to create intuitive and engaging user experiences.
+    I bring a unique combination of technical proficiency and creative flair to every project I undertake, from concept to deployment.
+    `,
+  experience: [
+    {
+      name: "Software Developer",
+      company: "Miovision",
+      location: "Kitchener, ON, Canada",
+      startDate: "July 2021",
+      endDate: "Present",
+      description: [],
+    },
+    {
+      name: "Full-stack Developer",
+      company: "Senstar",
+      location: "Kitchener, ON, Canada",
+      startDate: "January 2020",
+      endDate: "July 2021",
+      description: [
+        "✓ Implemented innovative features for 3 products using a combination of C#, Knockout, React.JS, and Python technologies.",
+        "✓ Successfully ported hardware changes to a new platform.",
+        "✓ Provided technical support to ensure seamless operation for clients.",
+        "✓ Collaborated with cross-functional teams to deliver high-quality products through the entire software development life cycle.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Systems Analyst Co-op",
+      company: "Sheridan College",
+      location: "Oakville, ON, Canada",
+      startDate: "Sepetember 2016",
+      endDate: "December 2019",
+      description: [
+        "Advanced Diploma of Computer Systems Technology – Systems Analyst",
+      ],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/reecerose",
+    email: "mailto:reecerose18@gmail.com",
+    linkedin: "https://www.linkedin.com/in/reece-rose-5b2864116/",
+  },
+
+  projects: [
+    // {
+    //   title: "Placeholder",
+    //   thumbnail: "https://via.placeholder.com/150",
+    //   githubUrl: "https://github.com",
+    //   liveUrl: "https://vercel.app/",
+    //   tech: [
+    //     {
+    //       title: "Astro",
+    //       colour: "bg-orange-400",
+    //     },
+    //   ],
+    // },
+  ],
+};
